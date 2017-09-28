388c06754f0640d7cbd79d3483aa2f7427983ab9
florian Bellazouz <florian.bellazouz@gmail.com>
2017-09-28 08:51:14 +0200

    add mongo_client_tests - reconnection

--- test/functional/mongo_client_tests.js
+++ test/functional/mongo_client_tests.js
@@ -150,12 +150,12 @@ exports['Should fail connection'] = {
   test: function(configuration, test) {
     configuration.stop(function() {
       var MongoClient = configuration.require.MongoClient;
-      var isFail = /failed to connect to server/.test
+      var isFail = /failed to connect to server/
       MongoClient.connect(configuration.url(), {
         autoReconnect:true,
         reconnectInterval: 1000,
       }, function(err, db) {
-        if(err) test.equal(true, isFail(err.message));
+        if(err) test.equal(true, isFail.test(err.message));
         configuration.start(test.done);
       });
     })
